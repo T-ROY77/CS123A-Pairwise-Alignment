c3acc452cb3efeadac5a563b09d3d23d9a6d8d1d
Troy <troy,perez@sjsu.edu>
2023-04-06 06:53:13 -0700

    UI

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,71 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
+        Grid grid = new Grid();
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Input sequence 1: ");
-        String seq1 = input.nextLine();
-        System.out.println("Input sequence 2: ");
-        String seq2 = input.nextLine();
-
-        System.out.println(seq1);
-        System.out.println(seq2);
-
-        //take ui
-        //c
-
-        String sequence1 = "hytdcdcdchtttk";
-        String sequence2 = "ioacddtfdsfsttp";
-
-        char[] seq1Array = sequence1.toCharArray();
-        char[] seq2Array = sequence2.toCharArray();
-
-        Grid grid = new Grid(seq1Array, seq2Array);
-        System.out.println("\n");
+        String seq1 = "";
+        String seq2 = "";
+        boolean stop = false;
 
 
-        grid.findAlignment();
 
-        //print statements
-        grid.printGrid();
-        System.out.println("\n");
-        grid.printArrowGrid();
-        System.out.println("\n");
-        grid.printAlignment();
-        System.out.println("\nMax score:");
-        System.out.println(grid.getMaxScore());
+        while (true) {
+            char[] seq1Array = seq1.toCharArray();
+            char[] seq2Array = seq2.toCharArray();
+            grid = new Grid(seq1Array, seq2Array);
+            System.out.println("\n");
 
+
+            System.out.println("Sequence 1: " + seq1);
+            System.out.println("Sequence 2: " + seq2);
+            System.out.println("1: Calculate an Optimal Alignment");
+            System.out.println("2: Print grid");
+            System.out.println("3: Print arrow grid");
+            System.out.println("4: Enter new query sequences");
+            System.out.println("5: quit");
+            String choice = input.nextLine();
+
+            try {
+                int inputNumber = Integer.parseInt(choice);
+                if (inputNumber == 1) {
+                    grid.findAlignment();
+                    grid.printAlignment();
+                    System.out.println("\nMax score:");
+                    System.out.println(grid.getMaxScore());
+                    inputNumber = 0;
+
+                }
+                else if(inputNumber == 2){
+                    grid.printGrid();
+                    inputNumber = 0;
+                }
+                else if(inputNumber == 3) {
+                    grid.printArrowGrid();
+                    inputNumber = 0;
+
+                }
+                else if (inputNumber == 4) {
+
+                        System.out.print("Enter the first value: ");
+                        String firstInput = input.nextLine();
+                        seq1 = firstInput;
+                        System.out.print("Enter the second value: ");
+                        String secondInput = input.nextLine();
+                        seq2 = secondInput;
+
+                }
+                else if(inputNumber == 5){
+                    break;
+                }
+                else {
+                    System.out.println("Invalid input.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number.");
+            }
+        }
+            System.out.println("Program stopped by user.");
     }
+
+
 }
