7d4d0fea389f0be17e2e921e1f987f7eb4ac26b7
Troy Perez <troy.perez@sjsu.edu>
2023-04-06 12:33:52 -0700

    setUpMatrix start

--- src/BLOSUMGrid.java
+++ src/BLOSUMGrid.java
@@ -17,10 +17,23 @@ public class BLOSUMGrid extends Grid{
 
     public void setUpMatrix() {
         System.out.println("test map");
+//        char[] match = new char[2];
+//        match[0] = 'a';
+//        match[1] = 'b';
+//        BLOSUM = new HashMap<char[], Integer>();
+//        BLOSUM.put(match, new Integer(2));
+
+
+
+        //enter scores from top left going down
+        //loop through nucleotides 'cc', 'cs', 'ct', 'cp'
+        //after one loop, start one more index later(skip c)
+        char[] nucleotides = new char[]{'c', 's', 't', 'p', 'a', 'g', 'n', 'd', 'e', 'q', 'h', 'r', 'k', 'm', 'i', 'l', 'v', 'f', 'y', 'w'};
+        int[] scores = new int[]{1,2};
         char[] match = new char[2];
-        match[0] = 'a';
-        match[1] = 'b';
-        BLOSUM = new HashMap<char[], Integer>();
-        BLOSUM.put(match, new Integer(2));
+        match[0] = nucleotides[0];
+        match[1] = nucleotides[0];
+        BLOSUM.put(match, new Integer(scores[0]));
+
     }
 }
