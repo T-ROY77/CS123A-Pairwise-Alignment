e9a4742f241ed2810c4a883d311353cb332a1d89
Troy Perez <troy.perez@sjsu.edu>
2023-04-06 14:11:40 -0700

    getMatchScore map problem

--- src/BLOSUMGrid.java
+++ src/BLOSUMGrid.java
@@ -11,17 +11,23 @@ public class BLOSUMGrid extends Grid{
         //need to error check that all chars are on matrix
 
         super(seq1, seq2);
+        setUpMatrix();
+
     }
 
     public BLOSUMGrid(){};
     @Override
     public int getMatchScore(char[] match) throws Exception {
-        if(BLOSUM.containsKey(match)){
-            return BLOSUM.get(match);
-        }
-        else{
-            throw new Exception("Key not found in Matrix");
-        }
+//        if(BLOSUM.containsKey(match)){
+//            return BLOSUM.get(match);
+//        }
+//        else{
+//            throw new Exception("Key not found in Matrix");
+//        }
+        int score = BLOSUM.get(match);
+        score = score +1;
+        return score;
+
     }
 
     public void setUpMatrix() {
@@ -39,20 +45,32 @@ public class BLOSUMGrid extends Grid{
         //after one loop, start one more index later(skip c)
 
         char[] nucleotides = new char[]{'c', 's', 't', 'p', 'a', 'g', 'n', 'd', 'e', 'q', 'h', 'r', 'k', 'm', 'i', 'l', 'v', 'f', 'y', 'w'};
-        int[] scores = new int[]{1,2};
-
+        int[] scores = new int[211];
+        for(int i = 0; i < 211; i++){
+            scores[i] = i;
+        }
         BLOSUM = new HashMap<char[], Integer>();
 
         int start = 0;
+        int scoreIndex = 0;
         for(int i = 0; i < nucleotides.length; i++){
             for(int j = start; j < nucleotides.length; j++){
                 char[] match = new char[2];
                 match[0] = nucleotides[i];
                 match[1] = nucleotides[j];
-                BLOSUM.put(match, new Integer(i + j));
+                BLOSUM.put(match, new Integer(scores[scoreIndex]));
+                scoreIndex++;
             }
             start++;
         }
-        System.out.println("" + BLOSUM);
+        System.out.println("number of scores: " + scoreIndex);
+
+
+        for (char[] variableName : BLOSUM.keySet())
+        {
+            char[] variableKey = variableName;
+            Integer variableValue = BLOSUM.get(variableName);
+            System.out.println(" " + variableKey[0] + variableKey[1] + " = " + variableValue);
+        }
     }
 }
